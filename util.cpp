985c1c7e2532c096fc325175ddfddb610d2ea50f
DomPazz <dompazz@yahoo.com>
2021-07-10 15:17:14 -0400

    remove unused params in config.  attempt to fix util (fail)

--- util.cpp
+++ util.cpp
@@ -167,7 +167,8 @@ void gpulog(int prio, int thr_id, const char *fmt, ...)
 		return;
 
 	if (gpu_threads > 1)
-		len = snprintf(pfmt, 128, "CPU T%d: Verus Hashing", thr_id, fmt);
+		len = snprintf(pfmt, 128, "CPU T%d: Verus Hashing %s", thr_id, fmt);
+		// len = snprintf(pfmt, 128, "Hello...");
 	else
 		len = snprintf(pfmt, 128, "GPU #%d: %s", dev_id, fmt);
 	pfmt[sizeof(pfmt)-1]='\0';
